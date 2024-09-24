942ae381a3f5c750170547a637289e4ab4fbb098
Kat-Moss <m012237n@student.staffs.ac.uk>
2024-09-24 21:35:37 +0100

    basic

---
+++ Source/m012237nC/Private/BasicActor.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasicActor.h"
+
+#include "Components/ArrowComponent.h"
+#include "Preferences/PersonaOptions.h"
+
+
+ABasicActor::ABasicActor()
+{
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_Mesh->SetupAttachment(_Root);
+
+	_Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	_Arrow->SetupAttachment(_Root);
+}
+
+
---
+++ Source/m012237nC/Private/BasicActor.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BasicActor.generated.h"
+
+class UArrowComponent;
+
+UCLASS(Abstract)
+class M012237NC_API ABasicActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	
+	ABasicActor();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> _Root;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> _Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UArrowComponent> _Arrow;
+};
