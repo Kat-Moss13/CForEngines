f6f0959f35a70fd79ec9300776411c94de873908
Kat-Moss13 <145793473+Kat-Moss13@users.noreply.github.com>
2024-10-01 11:49:44 +0100

    week 1 session1

---
+++ Source/m012237nC/Private/m012237nC_PC_DEBUG.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "m012237nC_PC_DEBUG.h"
+#include "EnhancedInputComponent.h"
+
+void Am012237nC_PC_DEBUG::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(_DebugAction, ETriggerEvent::Triggered, this, &Am012237nC_PC_DEBUG::Debug);
+	}
+}
+
+
+void Am012237nC_PC_DEBUG::Debug(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Display, TEXT("Debug working"));
+}
---
+++ Source/m012237nC/Private/m012237nC_PC_DEBUG.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "m012237nC_PC_DEBUG.generated.h"
+
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS(Abstract)
+class M012237NC_API Am012237nC_PC_DEBUG : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UPROPERTY(EditAnywhere,Category = "Input")
+	TObjectPtr<UInputAction> _DebugAction;
+
+	virtual void SetupInputComponent() override;
+
+	void Debug(const FInputActionValue& Value);
+	
+};
+
+
